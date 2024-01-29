9cdfddaa4b431842d5f681d49945127c713e1f8b
Arun Govind M <arungovindm@gmail.com>
2024-01-29 21:27:53 +0530

    CSS for disabled state, removes random color property for graph

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import DataTable from "./components/DataTable";
+import DataTable from './components/DataTable';
 
 function App() {
   return <DataTable />;
--- src/components/BarGraph.jsx
+++ src/components/BarGraph.jsx
@@ -1,28 +1,19 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 import Plot from 'react-plotly.js';
 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function BarGraph({ x, y, title }) {
+function BarGraph(props) {
   return (
     <Plot
       data={[
         {
-          x: x,
-          y: y,
+          x: props.x,
+          y: props.y,
           type: 'bar',
-          marker: { color: getRandomColor() },
         },
       ]}
-      layout={{ width: '100%', height: '100%', title: title }}
+      layout={{ width: '100%', height: '100%', title: props.title }}
       style={{ minWidth: '100%', minHeight: '100%' }}
     />
   );
